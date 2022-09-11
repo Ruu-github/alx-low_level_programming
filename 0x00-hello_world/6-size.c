c9897345bed15de8d41df42e0f8c5cebea23d8c3
Rufaro Razuwika <razuwika@gmail.com>
2022-09-11 11:35:16 -0700

    qn 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 
 	printf("Size of a char: %i byte(s)\n", sizeof(c));
 	printf("Size of an int: %i byte(s)\n", sizeof(i));
-	printf("Size of long int: %i byte(s)\n", sizeof(li));
-	printf("Size of long long int: %i byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %i byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(lli));
 	printf("Size of a float: %i byte(s)\n", sizeof(f));
 	return (0);
 }
